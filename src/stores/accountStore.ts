092e11675ba2ef75349abbe98c24a57f5866c08a
oleg <zustzero123@gmail.com>
2025-03-04 23:25:19 +0300

    implented updateAccountField, initialize, addAccount, removeAccount, updateAccount and save methods of store

--- src/stores/accountStore.ts
+++ src/stores/accountStore.ts
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { guidGenerator } from "@/helpers/guidGenerator";
 
 export interface AccountLabel {
   text: string;
@@ -21,5 +22,37 @@ export const useAccountStore = defineStore("accounts", {
   state: () => ({
     accounts: [] as Account[],
   }),
-  actions: {},
+  actions: {
+    updateAccountField(id: string, field: Partial<Account>) {
+      this.accounts = this.accounts.map((acc) => (acc.id === id ? { ...acc, ...field } : acc));
+      this.save();
+    },
+    initialize() {
+      const saved = localStorage.getItem("accounts");
+      if (saved) this.accounts = JSON.parse(saved);
+    },
+    addAccount() {
+      this.accounts.push({
+        id: guidGenerator(),
+        labels: [],
+        type: "LDAP",
+        login: "",
+        password: null,
+        errors: { login: false, password: false, labels: false },
+      });
+      this.save();
+    },
+    removeAccount(id: string) {
+      this.accounts = this.accounts.filter((acc) => acc.id !== id);
+      this.save();
+    },
+    updateAccount(account: Account) {
+      const index = this.accounts.findIndex((a) => a.id === account.id);
+      if (index > -1) this.accounts.splice(index, 1, account);
+      this.save();
+    },
+    save() {
+      localStorage.setItem("accounts", JSON.stringify(this.accounts));
+    },
+  },
 });
