cb8b7bae2fb79b005f4428b663c38d53e9ba6789
oleg <zustzero123@gmail.com>
2025-03-05 18:33:26 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<template></template>
+<template>
+  <AccountForm />
+</template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import AccountForm from "./components/AccountForm.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AccountForm,
+  },
+});
+</script>
 
 <style lang="scss"></style>
